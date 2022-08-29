c4b66599f9176ed0616e5dda8c1b498d17738de1
db360 <da.b@hotmail.es>
2022-08-29 17:44:19 +0200

    table, formato fecha, importes

--- components/Table.jsx
+++ components/Table.jsx
@@ -1,19 +1,24 @@
 import { useCallback, useState } from "react";
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
+import moment from 'moment';
 import data from "../public/datos_prueba.json";
 
-function sortData({tableData, sortKey, reverse}) {
-  if(!sortKey ) return tableData;
+function sortData({ tableData, sortKey, reverse }) {
+  if (!sortKey) return tableData;
 
   const sortedData = data.sort((a, b) => {
-    return a[sortKey] > b[sortKey] ? 1 : -1
-  })
+    return a[sortKey] > b[sortKey] ? 1 : -1;
+  });
 
-  if(reverse) {
-    return sortedData.reverse()
+  if (reverse) {
+    return sortedData.reverse();
   }
 
-  return sortedData
+  return sortedData;
+}
+
+function SortButton({ sortOrder, columnKey, sortKey, onClick }) {
+  return (<button onClick={onClick} className={`${sortKey === columnKey && sortOrder === 'desc' ? 'sort-button sort-reverse' : 'sort-button'}`}>🔼</button>)
 }
 
 function DataTable({ data }) {
@@ -33,29 +38,67 @@ function DataTable({ data }) {
     () => sortData({ tableData: data, sortKey, reverse: sortOrder === "desc" }),
     [data, sortKey, sortOrder]
   );
+
+  // console.log(sortedData())
+
+  function changeSort(key) {
+    setSortOrder(sortOrder === 'ascn' ? 'desc' : 'ascn')
+    setSortKey(key);
+  };
+
+  const formattedData = sortedData().map((data) => {
+
+    const fechaFormat = moment(data.fecha, "DD/MM/YYYY", true).format("DD/MM/YYYY");
+    const importe = parseFloat(data.importe.replace(",", ""));
+
+    return {
+      importe,
+      fechaFormat,
+      name: data.name,
+      contrato: data.objetivo_contrato,
+      status: data.status,
+      tipo: data.tipo
+    };
+
+  });
+
+  console.log({formattedData});
+
   return (
     <div>
       <Table className="">
         <Thead>
-          <Tr>
+          <Tr className="">
             {headers.map((row) => {
-              return <Td className="text-cyan-800 font-bold text-xl text-center" key={row.key}>{row.label}</Td>;
+              return (
+                <Td
+                  className="text-cyan-800 font-bold text-center"
+                  key={row.key}
+                >
+                  {row.label}
+                  <SortButton
+                    columnKey={row.key}
+                    onClick={() => changeSort(row.key)}
+                    {...{
+                      sortOrder,
+                      sortKey
+                    }}
+                  />
+                </Td>
+              );
             })}
           </Tr>
         </Thead>
         <Tbody>
-          {sortedData().map((dato) => (
-            <Tr className="hover:bg-gray-100 md:h-28" key={dato.name}>
-              <Td className="text-cyan-700 font-bold text-center">
-                {dato.name}
-              </Td>
-              <Td className="text-center">{dato.tipo}</Td>
-              <Td className="text-left">{dato.objetivo_contrato}</Td>
-              <Td className="text-center">{dato.status}</Td>
-              <Td className="text-center">{dato.importe}€</Td>
-              <Td className="text-center">{dato.fecha}</Td>
-            </Tr>
-          ))}
+          { formattedData.map(data =>
+            <Tr className="hover:bg-gray-100 md:h-28" key={data.name}>
+              <Td className="text-cyan-700 font-bold text-center"> {data.name}</Td>
+              <Td className="text-center">{data.tipo}</Td>
+              <Td className="text-left">{data.contrato}</Td>
+              <Td className="text-center">{data.status}</Td>
+              <Td className="text-center">{data.importe}€</Td>
+              <Td className="text-center">{data.fechaFormat}</Td>
+            </Tr>)}
         </Tbody>
       </Table>
     </div>
